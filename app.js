debac42e049706050508c8bf8aef6094f8d0f797
Whoppa <joeynalvarado@gmail.com>
2023-01-09 04:45:32 -0600

    add

---
+++ app.js
@@ -0,0 +1,21 @@
+const about = document.querySelector('.about');
+const btns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove active from all the buttons and add to target
+    btns.forEach(function (btn) {
+      btn.classList.remove('active');
+      e.target.classList.add('active');
+    });
+    // hide all articles
+    articles.forEach(function (article) {
+      article.classList.remove('active');
+    });
+    const element = document.getElementById(id);
+    console.log(element);
+    element.classList.add('active');
+  }
+});
